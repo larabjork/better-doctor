8d1db333aaad660ea277cbbeb8ea3d30b2d5718f
Lara Bjork <lara.m.bjork@gmail.com>
2019-11-22 09:37:13 -0800

    add draft version of API call to js files

--- src/main.js
+++ src/main.js
@@ -1,18 +1,33 @@
-// import { FUNCTION } from './PROJECT NAME';
+import { Symptoms } from './project.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 //
-// $(document).ready(function() {
-//   $('#ping-pong-form').submit(function(event) {
-//     event.preventDefault();
-//     var goal = $('#goal').val();
-//     var output = pingPong(goal);
-//     output.forEach(function(element) {
-//       $('#solution').append("<li>" + element + "</li>");
-//     });
-//   });
-// });
+$(document).ready(function() {
+  $('.symptom-form').submit(function(event) {
+    event.preventDefault();
+    const inputtedSymptom = $("#symptom").val();
+    $('#symptom').val("");
 
-var beverage = $("#beverage").val();
+    (async () => {
+         let mySymptoms = new Symptoms(inputtedSymptom);
+         let response = await mySymptoms.apiSymptoms(name);
+         // if (response.error) {
+         //   showError(response)
+         //   console.log(response);
+         // } else {
+         //   console.log(response);
+         getElements(response);
+       })();
+
+      function getElements(response) {
+         $('.confirm-symptom').text(`Sorry to hear that you're experiencing this symptom: ${response.data.name}`);
+         // $('.showTotal').text(`The total number of nonstolen bikes in your area is ${response.non}`);
+       }
+
+       // function showError(response) {
+       // $('.showErrors').text(`I'm sorry, Dave, I don't think I can do that because:  ${response.error}`);
+       // };
+  });
+});
--- src/project.js
+++ src/project.js
@@ -1,1 +1,10 @@
-export function
+export class Symptoms {
+
+  async apiSymptoms() {
+     let response = await fetch(`https://api.betterdoctor.com/2016-03-01/conditions?&appid=${process.env.API_KEY}&fields=name`);
+     let jsonifiedResponse = await response.json();
+     console.log('back end check');
+     return jsonifiedResponse;
+   }
+
+}
